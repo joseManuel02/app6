ca6c20e9650f8d08a504948aa3e07f7d8903b45a
FherCopal <38224705+FherCopal@users.noreply.github.com>
2019-05-08 09:01:09 -0500

    1

---
+++ js/acciones.js
@@ -0,0 +1,26 @@
+//JavaScript Document
+
+$(document).ready(function (e) {
+	document.addEventListener ("deviceready",function(){
+		
+	$('#izquierda').on("swipeleft",function(){
+			navigator.notification.alert("Usted deslizo a la izquierda", function(){"Aplicacion ","Aceptar"});
+});
+
+$('#derecha').on("swiperight",function(){
+	navigator .notification. confirm("¿que quieres hacer ahora?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
+			
+			case 2:
+			navigator.notification.vibreate(1000);
+			break;
+		}
+	},"Aplicacion ","Beep,Vibrar,Cancelar");
+	
+});
+	},false);
+});
